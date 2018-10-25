0476d2c4f8f4fb9b4423c4fa2fe0f114e18f72e8
vinnyotach7 <vinnyotach7@gmail.com>
2018-10-25 14:47:41 +0300

    changes to my quote.ts

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,11 @@
 export class Quote {
-    public showDescription: boolean;
+  length: any;
+  splice: any;
     constructor(public id: number, public name: string, public description: string, public completeDate: Date) {
         this.showDescription = false;
     }
+    public showDescription: boolean;
+  push(quote: any): any {
+    throw new Error('Method not implemented.');
+  }
 }
