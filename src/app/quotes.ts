3547d40cd6c0e565ff6fd5f6eeb9d3a81cd82bee
vinnyotach7 <vinnyotach7@gmail.com>
2018-10-25 14:51:47 +0300

    changes to my quote.ts

---
+++ src/app/quotes.ts
@@ -0,0 +1,12 @@
+import { Quote } from './quote';
+
+export const  Quotes: Quote[] =
+ [
+    new Quote(1, 'The mind is everything', 'The best way out is always rough', new Date(2018, 3, 14)),
+    new Quote(2, 'The trick in life is learning how to deal with it', 'Our life is frittered away by detail', new Date(2018, 6, 9)),
+    new Quote(3, 'Be happy for this moment.This moment is your life.', 'Love the life you live', new Date(2018, 1, 12)),
+    new Quote(4, 'Life is too important to be taken seriously', 'Keep calm and carry on.', new Date(2018, 0, 18)),
+    new Quote(5, 'Get busy living or get busy dying.', 'Life is short,death is forever', new Date(2018, 2, 14)),
+    new Quote(6, 'It’s better to be a lion for a day than a sheep all your life', 'Life is short but wide', new Date(2018, 3, 14)),
+
+  ];
