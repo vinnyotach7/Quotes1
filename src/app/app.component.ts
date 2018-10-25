d935852f3a7c3199686496ddfba1e0643820b10b
vinnyotach7 <vinnyotach7@gmail.com>
2018-10-25 11:36:54 +0300

    changes to my typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'Quotes1';
+//   quotes = [
+//     new Quote(1, 'Do not take life too seriously. You will never get out of it alive. '),
+//     new Quote(2, 'The trick in life is learning how to deal with it'),
+//     new Quote(3, 'Be happy for this moment. This moment is your life.'),
+//     new Quote(4, 'Life is too important to be taken seriously'),
+//     new Quote(5, 'Get busy living or get busy dying.'),
+//     new Quote(6, 'It’s better to be a lion for a day than a sheep all your life'),
+
+// ];
 }
+
